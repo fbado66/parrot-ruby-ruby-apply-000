2beae59e95bd24d51e497139d81522092a81f905
Franklin Bado <fbado66@gmail.com>
2020-04-04 21:20:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot( *"Squakw!", string="Squawk!" )
+def parrot( string="Squawk!" )
   puts "#{String}"
 end
 
